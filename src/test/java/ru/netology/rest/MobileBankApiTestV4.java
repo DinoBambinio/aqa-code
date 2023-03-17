ba2b756f8c68da59844d7a14727f9712c3de7862
Dmitrii Andreev <andreev352@gmail.com>
2023-03-17 19:25:15 +0300

    add bild failed

--- src/test/java/ru/netology/rest/MobileBankApiTestV4.java
+++ src/test/java/ru/netology/rest/MobileBankApiTestV4.java
@@ -9,23 +9,21 @@ import static org.hamcrest.Matchers.equalTo;
 class MobileBankApiTestV4 {
     @Test
     void shouldReturnDemoAccounts() {
-      // Given - When - Then
-      // Предусловия
-      given()
-          .baseUri("http://localhost:9999/api/v1")
-      // Выполняемые действия
-      .when()
-          .get("/demo/accounts")
-      // Проверки
-      .then()
-          .statusCode(200)
-          .body(matchesJsonSchemaInClasspath("accounts.schema.json"))
-<<<<<<< HEAD
-              .body("[2].currency", equalTo("RUB"))
-              .body("[1].currency", equalTo("USD"))
-=======
-          
->>>>>>> 7f31c1179ac45408720fa964975173ca2cfc65bb
-      ;
+        // Given - When - Then
+        // Предусловия
+        given()
+                .baseUri("http://localhost:9999/api/v1")
+                // Выполняемые действия
+                .when()
+                .get("/demo/accounts")
+                // Проверки
+                .then()
+                .statusCode(200)
+                .body(matchesJsonSchemaInClasspath("accounts.schema.json"))
+
+                .body("[2].currency", equalTo("RUR"))
+                .body("[1].currency", equalTo("USD"))
+
+        ;
     }
 }
